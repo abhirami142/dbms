5ec595a3b4e702f66d9d67d1bd56b70d2cc14d5b
abhiramia05 <abhiramia142@gmail.com>
2025-05-15 15:12:57 +0530

    Add files via upload

---
+++ trigger1.sql
@@ -0,0 +1,12 @@
+create or replace trigger t1 before insert or update or delete on customer for each row 
+when(new.id>0)
+declare
+	sal_diff number;
+begin
+	sal_diff:=:new.salary-:old.salary;
+	dbms_output.put_line('old salary'||:old.salary);
+	dbms_output.put_line('new salary'||:new.salary);
+	dbms_output.put_line('salary difference'||sal_diff);
+end;
+/
+	
