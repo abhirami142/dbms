19a691ff896e0d650db9033fec5633320f7a3fd4
abhiramia05 <abhiramia142@gmail.com>
2025-05-19 15:13:10 +0530

    Add files via upload

---
+++ trigger3.sql
@@ -0,0 +1,7 @@
+create or replace trigger t2 after insert on purchase for each row 
+when(new.noofitem>0)
+begin
+update stock set stock_available = stock_available-:new.noofitem where item =:new.item;
+dbms_output.put_line('stock updated');
+end;
+/
